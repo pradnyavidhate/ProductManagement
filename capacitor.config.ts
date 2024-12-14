b08e288f2c1011f04b9ab8a7242d997fae5308e8
Aniket Doke <aniket.doke@autoedgecorp.com>
2024-12-14 09:15:31 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'productmanagement',
+  webDir: 'www'
+};
+
+export default config;
